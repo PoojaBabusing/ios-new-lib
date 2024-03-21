dee746ff05b908d03ae4fb0269a5470d8cf70941
Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
2024-03-21 11:08:25 +0530

    Initial Commit

---
+++ test-framework-ios/test_framework_ios.h
@@ -0,0 +1,18 @@
+//
+//  test_framework_ios.h
+//  test-framework-ios
+//
+//  Created by Pooja Babusing on 21/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test_framework_ios.
+FOUNDATION_EXPORT double test_framework_iosVersionNumber;
+
+//! Project version string for test_framework_ios.
+FOUNDATION_EXPORT const unsigned char test_framework_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test_framework_ios/PublicHeader.h>
+
+
