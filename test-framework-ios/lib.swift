d6eb6e28308ae3392ade57d77c27d217661e6114
Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
2024-03-26 12:21:32 +0530

    add teps to generate xcFramework file

---
+++ test-framework-ios/lib.swift
@@ -0,0 +1,21 @@
+//
+//  lib.swift
+//  test-framework-ios
+//
+//  Created by Pooja Babusing on 21/03/24.
+//
+
+import Foundation
+
+public class lib {
+
+    public func multiply(_ a: Int, _ b: Int) -> Int {
+        var result = a * b;
+        print("This is coming from the lib ", result)
+        return result;
+    }
+
+}
+
+
+
